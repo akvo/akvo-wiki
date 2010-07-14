df49f4e42b59f06ef39e57d5079a7462a24a1454
Daniel Karlsson <daniel@fraction4.com>
2010-07-14 17:04:35 +0200

    Added footer and akvopedia logo.

--- Akvo.php
+++ Akvo.php
@@ -116,11 +116,12 @@ class AkvoTemplate extends QuickTemplate {
 		
 			#globalWrapper {
 				margin-top:45px;
-				font-size:14px;
+				font-size:13px;
+				line-height:19px;
 				}
 				
-			#p-cactions, #p-personal {
-				margin-top:45px;
+			#p-cactions, #p-personal, #p-logo {
+				margin-top:50px;
 				}
 			a { 
 			    color:#2B80AF;
@@ -135,6 +136,19 @@ class AkvoTemplate extends QuickTemplate {
 				margin-bottom:0.5em; 
 				margin-bottom:1.5em;   
 				margin-top:-0.5em;    
+			    }
+			
+			#footer {
+			    background:#F9F9FA url(<?php $this->text('stylepath') ?>/akvo/img/footer_gradient.png) repeat-x top;
+			    color:#585E68;
+			    padding-top:40px;
+				text-align:left;
+				font-size:14px;
+			    }
+
+			#footer h3, #footer h2  {
+			    color: #585E68;
+				border:none;
 			    }
 			
 		</style>
@@ -238,11 +252,9 @@ class AkvoTemplate extends QuickTemplate {
 		</div>
 	</div>
 	<div class="portlet" id="p-logo">
-		<?php /*
 		<a style="background-image: url(<?php $this->text('logopath') ?>);" <?php
 			?>href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"<?php
 			echo $skin->tooltipAndAccesskey('p-logo') ?>></a>
-		*/?>
 	</div>
 	<script type="<?php $this->text('jsmimetype') ?>"> if (window.isMSIE55) fixalpha(); </script>
 <?php
@@ -306,6 +318,74 @@ class AkvoTemplate extends QuickTemplate {
 
 -->
 <?php endif; ?>
+<div id="footer">
+	<div id="footer_container" class="container">
+		<div class="span-18">
+			<h2>About Akvo.org</h2>
+			<p>
+				We help donors and doers reach out to fund many thousands of new water and sanitation projects. Money flows quickly because donors choose what to fund and follow progress online. People can use these storylines to build exciting new campaigns and networks. Our partners lead the world in sharing updates using Akvo tools. Better feedback means happy donors, and dialogue between field workers builds skills and improves quality. Time and money are saved while people across the world get safe drinking water and proper sanitation.
+			</p>
+		</div>
+		<div class="span-18">
+			<div class="span-3 first">
+				<h3>Policies</h3>
+				<p class="small">
+					<a href="/web/terms_of_use">Terms of use</a><br />
+					<a href="/web/donation_policy">Donations</a><br />
+					<a href="/web/partners_and_projects_policy">Partners</a><br />
+					<a href="/web/user_contribution_policy">User Contribution Policy</a><br />
+					<a href="/web/open_license">Akvo licensing and copyrights</a><br />
+					<a href="/web/privacy_policy">Privacy</a><br />
+					<a href="/web/akvo-rsr-api-code-of-conduct">Akvo RSR API Code of Conduct</a><br />
+				</p>
+			</div>
+			<div class="span-3">
+				<h3>About</h3>
+				<p class="small">
+					<a href="/web/what_we_do">What we do</a><br />
+					<a href="/web/how_akvo_works">How Akvo works</a><br />
+					<a href="/web/why_we_do_it">Why we do it</a><br />
+					<a href="/web/faq">Akvo Frequently Asked Questions</a><br />
+					<a href="/web/map_partners_projects">Map</a><br />
+					<a href="/web/team">Team</a><br />
+					<a href="/web/jobs">Working at Akvo</a><br />
+					<a href="/web/press">Press</a><br />
+					<a href="/web/annual_reports">Akvo annual reports & Articles of incorporation</a><br />
+				</p>
+			</div>
+			<div class="span-3">
+				<h3>Partners</h3>
+				<p class="small">
+					<a href="http://akvo.org/web/partners">Strategic partners</a><br />
+					<a href="{% url rsr_org_list %}">Project partners</a><br />
+					<a href="/web/commercial_partners">Commercial partners</a><br />
+					<a href="/web/knowledge_institute_partners">Knowledge institute partners</a><br />
+					<a href="/web/akvo_investors">Akvo investors</a><br />
+				</p>
+			</div>
+			<div class="span-3">
+				<h3>Developers</h3>
+				<p class="small">
+					<a href="/labs/index.php/Main_Page">Akvo Labs</a><br />
+					<a href="http://github.com/akvo/">Source Code</a><br />
+				</p>
+			</div>
+			<div class="span-3">
+				<h3>Contact us</h3>
+				<p class="small">
+					<a href="http://help.akvo.org/">Help &amp; Support</a><br />
+					<a href="/web/contact_us">Contact us</a><br />
+				</p>
+			</div>
+			<div class="span-3">
+				&nbsp;
+			</div>
+			<div class="span-3 last">
+				
+			</div>
+		</div>
+	</div>
+</div>
 </body></html>
 <?php
 	wfRestoreWarnings();
